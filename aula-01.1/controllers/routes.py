f8153c193ed186df0a1bbe3b2b96008385c5d3c0
João Barreto <joaobarretoprof@gmail.com>
2025-02-17 22:21:51 -0300

    Adicionando o cadastro de jogos

--- aula-01.1/controllers/routes.py
+++ aula-01.1/controllers/routes.py
@@ -1,6 +1,11 @@
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 
 jogadores = []
+
+gamelist = [{'titulo': 'League of Legends',
+             'ano': 2009,
+             'categoria': 'Moba'}]
+
 
 def init_app(app):
 
@@ -10,11 +15,18 @@ def init_app(app):
 
     @app.route('/games', methods=['GET', 'POST'])
     def games():
-        game = {'Título': 'League of Legends',
-                'Ano': 2009,
-                'Categoria': 'Moba'}
-        
+        game = gamelist[0]
         if request.method == 'POST':
             if request.form.get('jogador'):
                 jogadores.append(request.form.get('jogador'))
+                return redirect(url_for('games'))
+
         return render_template('games.html', game=game, jogadores=jogadores)
+
+    @app.route('/cadgames', methods=['GET', 'POST'])
+    def cadgames():
+        if request.method == 'POST':
+            form_data = request.form.to_dict()
+            gamelist.append(form_data)
+
+        return render_template('cadgames.html', gamelist=gamelist)
